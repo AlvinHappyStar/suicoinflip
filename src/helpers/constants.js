3f47d2badbb7406d07cd4c33efbc657c12dde667
root <root@dream.com>
2022-12-02 19:03:52 +0300

    rpc fixed

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,6 +1,6 @@
 export const suiRpcUrl = 'https://fullnode.devnet.sui.io:443'
-export const casinoAddress ='0xf52c7a83d88ad4a0a5e190289c6f86cc0c6f2403';
-export const moduleAddress = '0x10beab4fa568d8a223bc09d40682edea325a6c21';
+export const casinoAddress ='0x94969585627d0e1ebda74c4af6be2796702ef10a';
+export const moduleAddress = '0xd85583bc7626a2f1a43a9a7470d00a15c444fa3d';
 export const moduleName = 'Base';
 export const localStorageKeys = {
     address: 'user_sui_address',
